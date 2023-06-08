91a8c5a2e9076683a7ea63ae4bfa5aa7bb561b0b
VIKAS VERMA <vikasvermaupvns@gmail.com>
2023-06-08 16:04:38 +0530

    some changes in index file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
@@ -6,15 +7,32 @@ import React, { useState } from 'react';
  
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+
+      showAlert(" Dark mode has been enabled", "success")
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+
+      showAlert(" light mode has been enabled", "success")
     }
   }
   return (
@@ -22,8 +40,9 @@ function App() {
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
     </div>
     </> 
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,11 +6,13 @@ export default function TextForm(props) {
         // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to uppercase", "success")
     }
 
     const handleLoClick = ()=>{ 
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to lowercase", "success")
     }
 
     const handleClearClick = ()=>{ 
