5e85f8d54710a8957aab7b6a96a6048acbf4dc43
VIKAS VERMA <vikasvermaupvns@gmail.com>
2023-06-09 16:57:48 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -2,50 +2,53 @@ import './App.css';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 import React, { useState } from 'react';
 
- 
+
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
 
 
-  const showAlert=(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
 
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null)
-    },1500)
+    }, 1500)
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
-
       showAlert(" Dark mode has been enabled", "success")
+
+      document.title = 'TextUtils - Dark Mode'
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
 
       showAlert(" light mode has been enabled", "success")
+      document.title = 'TextUtils - Light Mode'
     }
   }
   return (
     <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-    {/* <Navbar/> */}
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-    </div>
-    </> 
-  );
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      {/* <Navbar/> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+        </div>
+      </>
+      );
 }
 
-export default App;
+      export default App;
